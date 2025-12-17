33a8306b17979731ea8d95fab5c6aa358c6a2e04
Anurag Mishra <anuragmishra3407@gmail.com>
2025-12-18 01:16:38 +0530

    modified views for post

--- twitter/tweet/views.py
+++ twitter/tweet/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, redirect, render
+
+from twitter import tweet
 from .models import Tweet
 from .forms import TweetForm
 
+from django.shortcuts import get_object_or_404
+
 
 def index(request):
-    tweets = Tweet.objects.all().order_by('-created_at')
+    tweets = get_list_or_404(Tweet.objects.all().order_by('-created_at'))
     return render(request, 'index.html', {'tweets': tweets})
+
+
+def tweet_list(request):
+    tweets = get_list_or_404(Tweet.objects.all().order_by('-created_at'))
+    return render(request, 'tweet_list.html', {'tweets': tweets})
+
+def tweet_create ( request ):
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm()
+    return render(request, 'tweet_form.html', {'form': form})
+
+def tweet_edit(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id)
+    if request.method == 'POST':
+        form = TweetForm(request.POST, instance=tweet, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request, 'tweet_form.html', {'form': form})
+
+def tweet_delete(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})       
+
